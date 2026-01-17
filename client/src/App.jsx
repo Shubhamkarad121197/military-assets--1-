019ba20631b8cf79281706db771af41657900a2c
shubham karad <shubham.karad12@gmail.com>
2026-01-17 22:53:32 +0530

    pages path issue resolve

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import Purchases from "./Pages/Purchases";
+import Purchases from "./pages/Purchases";
 
 export default function App() {
   return (
